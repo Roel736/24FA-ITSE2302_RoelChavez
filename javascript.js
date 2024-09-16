0371ed3eec10e10cb34a6cb66744526ad2aa3e24
Roel736 <rchavez138912@mymail.tstc.edu>
2024-09-16 15:44:22 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+var firstname = "Roel";
+var lastname = "Chavez";
+var stringvariable = "It's raining dogs and cats!";
+let x = true;
+var theTruth = Boolean(x);
+var v1 = 10;
+var v2 = 15;
+var v3 = 25;
+
+console.log("My name is " + firstname + " " + lastname);
+console.log(stringvariable.length);
+console.log("The sum of " + v1 + " and " + v2 + " is equal to " + v3);
+console.log("The variable theTruth is storing the value " + Boolean(theTruth) + ". " + "The data type of theTruth is " + typeof true);
