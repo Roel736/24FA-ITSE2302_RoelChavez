529f58ab97863a1bff7fae0be5b03c0af6c5ca78
Roel736 <rchavez138912@mymail.tstc.edu>
2024-10-03 14:51:27 -0500

    Update javascript.js

--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -10,11 +10,13 @@ var v3 = 25;
 
 // says my name
 console.log("My name is " + firstname + " " + lastname);
-console.log(
   //   says how many words there are for raining cats and dogs
+console.log(
   "The length of my sentence is: " + stringvariable.length + " characters long."
 );
+  // Values added up
 console.log("The sum of " + v1 + " and " + v2 + " is equal to " + v3);
+// Boolean
 console.log(
   "The variable theTruth is storing the value " +
     Boolean(theTruth) +
