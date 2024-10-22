130a44566e4edcff7351f4660858fa88970b71f6
Roel736 <rchavez138912@mymail.tstc.edu>
2024-10-22 15:54:31 -0500

    Add files via upload

---
+++ 24FA-ITSE2302_RoelChavez-Module-3/JavaScript/javascript.js
@@ -0,0 +1,26 @@
+// first and last name
+var firstname = "Roel";
+var lastname = "Chavez";
+var stringvariable = "It's raining dogs and cats!";
+let x = true;
+var theTruth = Boolean(x);
+var v1 = 10;
+var v2 = 15;
+var v3 = 25;
+
+// says my name
+console.log("My name is " + firstname + " " + lastname);
+  //   says how many words there are for raining cats and dogs
+console.log(
+  "The length of my sentence is: " + stringvariable.length + " characters long."
+);
+  // Values added up
+console.log("The sum of " + v1 + " and " + v2 + " is equal to " + v3);
+// Boolean
+console.log(
+  "The variable theTruth is storing the value " +
+    Boolean(theTruth) +
+    ". " +
+    "The data type of theTruth is " +
+    typeof true
+);
