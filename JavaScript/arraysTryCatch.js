fed41b228d7ce8c4b77363b57397189dbf29ab84
Roel736 <rchavez138912@mymail.tstc.edu>
2024-11-01 15:57:47 -0500

    Add files via upload

--- JavaScript/arraysTryCatch.js
+++ JavaScript/arraysTryCatch.js
@@ -1,7 +1,12 @@
+//Cat variables! ( car is a hip and new cool way to say cat )
 var Stopics = ["Maine coons", "Tuxedo", "Car", "russian blue", "Laperm" ];
+//weekends and weekday names!
 var dayNames  = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+//name
 var fname = "Roel";
+//date of birth
 var dateofbirth = 2009;
+//adds a new cat breed
  Stopics.push("Selkirk Rex");
 console.log("The length of the siteTopics array is " + Stopics.length + ".");
 var i = 0;
@@ -10,7 +15,9 @@ while (i < Stopics.length) {
     console.log(Stopics[i]);
     i++;
   }
+ //reverses day names
   dayNames.reverse();
+  //takes a daynameout
   dayNames.shift();
   // console.log(dayNames);
   
@@ -18,7 +25,7 @@ while (i < Stopics.length) {
     {
       
       if(dayNames[0] == "Sunday"){
-
+//error !
         throw "Error: Sunday should be missing!";
       }
       
